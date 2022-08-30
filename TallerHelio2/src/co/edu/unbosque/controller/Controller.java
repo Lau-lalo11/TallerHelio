181d9657311a16bf487e36b99dc0abef2eb37e63
USER <USER@Lau>
2022-08-29 19:16:55 -0500

    Quicksort

--- TallerHelio2/src/co/edu/unbosque/controller/Controller.java
+++ TallerHelio2/src/co/edu/unbosque/controller/Controller.java
@@ -1,19 +1,22 @@
 package co.edu.unbosque.controller;
 import co.edu.unbosque.model.FuncionDeOrdenamiento;
+import co.edu.unbosque.model.Quicksort;
 import co.edu.unbosque.view.Vista;
 
 public class Controller {
 	
 	private FuncionDeOrdenamiento funcionOrdenamiento;
+	private Quicksort quick;
 	private Vista vista;
 	
 	public Controller() {
 		
 		vista = new Vista();
+		quick = new Quicksort();
 		funcionOrdenamiento = new FuncionDeOrdenamiento();
 		
 		
-		int[] numero=funcionOrdenamiento.cocktail_Sort(generarNumerosAleatorios(vista.obtenerDato("Ingresar el tamano que va a poner a prueba")));
+		int[] numero=quick.OrganizarDato(generarNumerosAleatorios(vista.obtenerDato("Ingresar el tamano que va a poner a prueba")));
 		for(int x=0;x<numero.length;x++) {
 			System.out.println(x+" - "+numero[x]);
 		}
---
+++ TallerHelio2/src/co/edu/unbosque/model/Quicksort.java
@@ -0,0 +1,39 @@
+package co.edu.unbosque.model;
+
+public class Quicksort {
+
+	public int particion(int arreglo[], int izquierda, int derecha) {
+		int pivote = arreglo[izquierda];
+		while (true) {
+			while (arreglo[izquierda] < pivote) {
+				izquierda++;
+			}
+			while (arreglo[derecha] > pivote) {
+				derecha--;
+			}
+			if (izquierda >= derecha) {
+				return derecha;
+			} else {
+				int temporal = arreglo[izquierda];
+				arreglo[izquierda] = arreglo[derecha];
+				arreglo[derecha] = temporal;
+				izquierda++;
+				derecha--;
+			}
+		}
+	}
+
+	public int[] quicksort(int arreglo[], int izquierda, int derecha) {
+		if (izquierda < derecha) {
+			int indiceParticion = particion(arreglo, izquierda, derecha);
+			quicksort(arreglo, izquierda, indiceParticion);
+			quicksort(arreglo, indiceParticion + 1, derecha);
+		}
+		return arreglo;
+	}
+
+	public int[] OrganizarDato(int arreglo[]) {
+		return quicksort(arreglo,0,arreglo.length-1);
+	}
+
+}
